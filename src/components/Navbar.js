fc0ca46018716cd6cd120156b606b0654f77ab87
lube-blue <85723519+lube-blue@users.noreply.github.com>
2026-03-05 18:06:54 +0700

    fix: resolve eslint comment errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,26 +13,26 @@ const Navbar = () => {
       <div className="nav-menu-tamal">
         <a href="#home">
           <span className="nav-num">01</span>
-          <span className="nav-text">// home</span>
+          <span className="nav-text">{"// home"}</span>
         </a>
         <a href="#about">
           <span className="nav-num">02</span>
-          <span className="nav-text">// about</span>
+          <span className="nav-text">{"// about"}</span>
         </a>
         <a href="#experience">
           <span className="nav-num">03</span>
-          <span className="nav-text">// experience</span>
+          <span className="nav-text">{"// experience"}</span>
         </a>
         
         {/* 🟢 แก้ตรงนี้ครับ เปลี่ยนจาก work เป็น education */}
         <a href="#education">
           <span className="nav-num">04</span>
-          <span className="nav-text">// education</span>
+          <span className="nav-text">{"// education"}</span>
         </a>
         
         <a href="#contact">
           <span className="nav-num">05</span>
-          <span className="nav-text">// contact</span>
+          <span className="nav-text">{"// contact"}</span>
         </a>
       </div>
     </nav>
